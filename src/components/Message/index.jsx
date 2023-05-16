c27297c71687452d776467e086b07cb32f86e0b2
Zlatko Furčić <zlatko.furcic@gmail.com>
2023-05-16 15:27:20 +0200

    left right display of messages done

--- src/components/Message/index.jsx
+++ src/components/Message/index.jsx
@@ -3,7 +3,7 @@ import './message.scss'
 const Message = ({m}) => {
   return(
     <li className={`message ${m.fromMe ? 'message--sent' : 'message--received'} `}>
-      {m.text} {m.member.clientData.username}
+      <span>{m.text} {m.member.clientData.username}</span>
     </li> 
   )
 }
