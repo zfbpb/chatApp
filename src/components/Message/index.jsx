3f01e99a0791d3cca266263f8e8ebc5c38a05da7
Zlatko Furčić <zlatko.furcic@gmail.com>
2023-05-16 16:04:24 +0200

    styles improved

--- src/components/Message/index.jsx
+++ src/components/Message/index.jsx
@@ -1,10 +1,18 @@
 import './message.scss'
 
 const Message = ({m}) => {
+  const messageClasses = `message message--${m.fromMe ? 'sent' : 'received'}`;
+  const containerClasses = 'message__container';
+  const senderClasses = 'message__sender';
+  const textClasses = 'message__text';
+
   return(
-    <li className={`message ${m.fromMe ? 'message--sent' : 'message--received'} `}>
-      <span>{m.text} {m.member.clientData.username}</span>
-    </li> 
+    <li className={messageClasses}>
+      <div className={containerClasses}>
+        <span className={senderClasses}>{m.member.clientData.username}</span>
+        <span className={textClasses}>{m.text}</span>
+      </div>
+    </li>
   )
 }
 
